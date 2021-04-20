2f05d3e12ada3feaf13cd3742a8f9a681acb8219
KBlackz <72504625+KBlackz@users.noreply.github.com>
2021-04-19 22:26:05 -0400

    Revert "Max"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
+
 import './App.css';
 import React, { useState, useEffect } from "react"
-import { Switch, Route } from "react-router-dom";
 import CarCollection from "./components/CarCollection"
-import NavBar from "./components/NavBar"
-import WishlistNames from "./components/WishlistNames"
-import WishlistDetails from "./components/WishlistDetails"
-import SignUp from "./components/SignUp"
-import Login from "./components/Login"
 
 const cars_url = "http://localhost:3000/cars"
 function App() {
@@ -21,22 +16,7 @@ function App() {
 
   return (
     <div >
-      <NavBar />
-      <Switch>
-        <Route exact path="/signup">
-          <SignUp />
-        </Route>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/wishlist">
-          <WishlistDetails />
-        </Route>
-        <Route exact path="/">
-            <CarCollection cars={cars} />
-        </Route>
-      </Switch>
-      <WishlistNames />
+        <CarCollection cars={cars} />
     </div>
   );
 }
--- src/components/WishlistNames.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-function WishlistNames() {
-    return(
-        <div> WishlistNames </div>
-    )
-}
-
-export default WishlistNames
